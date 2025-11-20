8949c03516022c0caf4fe1ec865f3e8ec3e02a67
yuo <2685245554@qq.com>
2025-11-20 21:12:53 +0800

    攻速，暴击附魔

--- src/main/java/com/yuo/Enchants/RlUtils.java
+++ src/main/java/com/yuo/Enchants/RlUtils.java
@@ -1,18 +1,23 @@
 package com.yuo.Enchants;
 
+import net.minecraft.ResourceLocationException;
 import net.minecraft.resources.ResourceLocation;
 
 public class RlUtils {
 
     public static ResourceLocation fa(String path){
-        return ResourceLocation.fromNamespaceAndPath(YuoEnchants.MOD_ID, path);
+        return new ResourceLocation(YuoEnchants.MOD_ID, path);
     }
 
     public static ResourceLocation wdn(String path){
-        return ResourceLocation.withDefaultNamespace(path);
+        return new ResourceLocation(path);
     }
 
     public static ResourceLocation tryParse(String path){
-        return ResourceLocation.tryParse(path);
+        try {
+            return new ResourceLocation(path);
+        } catch (ResourceLocationException var2) {
+            return null;
+        }
     }
 }
